9450e8b3b191e7e6a28df747632cea13d535ee1f
abdo-essam <abdo.e.mosa@gmail.com>
2025-10-05 17:26:07 +0300

    refactor(ui): Rename layout_section_header to section_header
    The layout file `layout_section_header.xml` was renamed to
    `section_header.xml` for consistency. Corresponding references in
    `HomeFragment.kt` and `fragment_home.xml` were updated accordingly.

--- app/src/main/java/com/qurio/trivia/ui/home/HomeFragment.kt
+++ app/src/main/java/com/qurio/trivia/ui/home/HomeFragment.kt
@@ -17,7 +17,7 @@ import com.qurio.trivia.data.model.GameResult
 import com.qurio.trivia.data.model.UserProgress
 import com.qurio.trivia.databinding.FragmentHomeBinding
 import com.qurio.trivia.databinding.ItemStreakBinding
-import com.qurio.trivia.databinding.LayoutSectionHeaderBinding
+import com.qurio.trivia.databinding.SectionHeaderBinding
 import com.qurio.trivia.databinding.LayoutTopBarBinding
 import com.qurio.trivia.ui.adapters.CategoryAdapter
 import com.qurio.trivia.ui.adapters.LastGamesAdapter
@@ -43,12 +43,12 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomePresenter>(), HomeVie
         ItemStreakBinding.bind(binding.root.findViewById(R.id.layout_streak))
     }
 
-    private val sectionHeaderGamesBinding: LayoutSectionHeaderBinding by lazy {
-        LayoutSectionHeaderBinding.bind(binding.root.findViewById(R.id.section_header_games))
+    private val sectionHeaderGamesBinding: SectionHeaderBinding by lazy {
+        SectionHeaderBinding.bind(binding.root.findViewById(R.id.section_header_games))
     }
 
-    private val sectionHeaderLastGamesBinding: LayoutSectionHeaderBinding by lazy {
-        LayoutSectionHeaderBinding.bind(binding.root.findViewById(R.id.section_header_last_games))
+    private val sectionHeaderLastGamesBinding: SectionHeaderBinding by lazy {
+        SectionHeaderBinding.bind(binding.root.findViewById(R.id.section_header_last_games))
     }
 
     private val categoryAdapter by lazy {
