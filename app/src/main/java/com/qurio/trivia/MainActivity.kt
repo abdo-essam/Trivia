6317005b6c8bf7083b0a11b8c8f290a62e7a25c1
abdo-essam <abdo.e.mosa@gmail.com>
2025-10-10 17:08:15 +0300

    feat: Implement splash screen and navigation
    Implement splash screen to handle initial app loading
    and navigation to either the onboarding or home screen
    based on first launch status. Replaces LoadingFragment.

--- app/src/main/java/com/qurio/trivia/MainActivity.kt
+++ app/src/main/java/com/qurio/trivia/MainActivity.kt
@@ -2,19 +2,44 @@ package com.qurio.trivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.qurio.trivia.databinding.ActivityMainBinding
+import com.qurio.trivia.utils.PreferenceKeys.IS_FIRST_LAUNCH
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Hide action bar
         supportActionBar?.hide()
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val isFirstLaunch = intent.getBooleanExtra(
+            IS_FIRST_LAUNCH,
+            true
+        )
+
+        val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
+        val startDestination = if (isFirstLaunch) {
+            R.id.onboardingFragment
+        } else {
+            R.id.homeFragment
+        }
+
+        navGraph.setStartDestination(startDestination)
+        navController.graph = navGraph
     }
 }
