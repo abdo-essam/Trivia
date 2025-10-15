ef32c289895c6fc2b63795a668021c4bd98cf0e5
abdo-essam <abdo.e.mosa@gmail.com>
2025-10-15 19:48:49 +0300

    refactor(ui): Refactor GameResultFragment
    Refactors GameResultFragment and presenter to improve
    code structure and readability. Also, injects the
    AchievementsRepository to unlock new achievements
    when game result is saved.

--- app/src/main/java/com/qurio/trivia/presentation/ui/result/GameResultView.kt
+++ app/src/main/java/com/qurio/trivia/presentation/ui/result/GameResultView.kt
@@ -2,11 +2,7 @@ package com.qurio.trivia.presentation.ui.result
 
 import com.qurio.trivia.presentation.base.BaseView
 
-/**
- * View contract for GameResultFragment
- */
 interface GameResultView : BaseView {
     fun navigateToHome()
     fun navigateToPlayAgain()
-    fun onGameResultSaved(coins: Int, stars: Int)
 }
